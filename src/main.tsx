203afc7b7d9ec05d3f430dcaf1cb73dae3f8b99f
Nestor Carvantes <nestorba@amazon.com>
2024-05-07 07:35:29 -0700

    finished demo

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
 import outputs from '../amplify_outputs.json'
-import { Authenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(outputs);
 
